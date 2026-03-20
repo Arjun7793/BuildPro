9c3396c310f523cab51af248b22fd7443ded2e0c
vuhapotharlanka <Vpotharlanka@midwesttape.com>
2026-03-21 02:44:48 +0530

    initial commit

---
+++ src/test/java/com/example/sample_starter/SampleStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
